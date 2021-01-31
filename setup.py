48c0316f8e90bb3474bd69e4159bcd52fd8cfda6
Regan Karlewicz <regan@dragonchain.com>
2021-01-30 20:16:25 -0800

    Change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='yfinance2pg-karlewr',  # Replace with your own username
+    name='yfinance2pg',  # Replace with your own username
     version='0.0.1',
     author='Regan Karlewicz',
     author_email='yfinance2pg@karlewr.net',
